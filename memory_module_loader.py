19157da600d5b15c72a6306c29afcdc3f599b8f5
casell <supercasell@gmail.com>
2014-06-06 10:03:22 +0200

    Changed license to BEER-WARE

--- memory_module_loader.py
+++ memory_module_loader.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+#
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# Casell wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return Casell
+# ----------------------------------------------------------------------------
+
 """Loads modules from base64 strings
 The expected format is:
     tuple(FORMAT, CONTENT)
